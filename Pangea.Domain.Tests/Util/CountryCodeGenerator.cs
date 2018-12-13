0d74b780d7342ea19e52eb56e1d148646985b0a1
Ruben Rorije <rubenrorije@hotmail.com>
2018-12-13 20:45:15 +0400

    Remove dead code Moved data in test to [DataRow]

--- Pangea.Domain.Tests/Util/CountryCodeGenerator.cs
+++ Pangea.Domain.Tests/Util/CountryCodeGenerator.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pangea.Domain.Tests
+namespace Pangea.Domain.Tests.Util
 {
     [TestClass]
     public class CountryCodeGenerator
