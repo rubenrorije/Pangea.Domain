18ce1d2bc6b00c545bd9c222ef223fac9cb86ab4
Ruben Rorije <rubenrorije@hotmail.com>
2019-01-28 13:45:12 +0330

    Add Isbn struct

---
+++ Pangea.Domain.Tests/IsbnTests.cs
@@ -0,0 +1,99 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangea.Domain.Tests
+{
+    [TestClass]
+    public class IsbnTests
+    {
+        [TestMethod]
+        public void Creating_A_Null_Isbn_Is_Allowed()
+        {
+            Action action = () => new Isbn(null);
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void Creating_An_Empty_Isbn_Is_Allowed()
+        {
+            Action action = () => new Isbn(string.Empty);
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        [DataRow(null, true)]
+        [DataRow("1", false)]
+        [DataRow("12", false)]
+        [DataRow("123", false)]
+        [DataRow("1234", false)]
+        [DataRow("12345", false)]
+        [DataRow("123456", false)]
+        [DataRow("1234567", false)]
+        [DataRow("12345678", false)]
+        [DataRow("123456789", false)]
+        [DataRow("1234567890", true)]
+        [DataRow("12345678901", false)]
+        [DataRow("123456789012", false)]
+        [DataRow("1234567890123", true)]
+        [DataRow("12345678901234", false)]
+        public void Length_Is_Correct(string text, bool result)
+        {
+            Action action = () => new Isbn(text);
+            if (result)
+            {
+                action.Should().NotThrow(text);
+            }
+            else
+            {
+                action.Should().Throw<ArgumentOutOfRangeException>(text);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("9780123456789", "0")]
+        [DataRow("9781123456789", "1")]
+        [DataRow("9782123456789", "2")]
+        [DataRow("9783123456789", "3")]
+        [DataRow("9784123456789", "4")]
+        [DataRow("9785123456789", "5")]
+        [DataRow("9786123456789", "612")]
+        [DataRow("9787123456789", "7")]
+        [DataRow("9788123456789", "81")]
+        [DataRow("9789123456789", "91")]
+        [DataRow("9789623456789", "962")]
+        [DataRow("9789923456789", "9923")]
+        [DataRow("9789993456789", "99934")]
+        public void Identifier_Group_Correctly_Extracted(string isbn, string identifierGroup)
+        {
+            var sut = new Isbn(isbn);
+            sut.IdentifierGroup.Should().Be(identifierGroup);
+        }
+
+        [TestMethod]
+        public void Hyphens_Are_Allowed()
+        {
+            Action action = () => new Isbn("978-0-123456789");
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void Only_Digits_And_Hyphens_Are_Allowed()
+        {
+            Action action = () => new Isbn("978-0-12345678a");
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [TestMethod]
+        public void ToString_Of_Null_Isbn_Is_Allowed()
+        {
+            new Isbn(null).ToString().Should().BeNull();
+        }
+
+
+    }
+}
---
+++ Pangea.Domain/Isbn.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Pangea.Domain
+{
+    /// <summary>
+    /// The International Standard Book Number (ISBN) is a numeric commercial book identifier which is intended to be unique.
+    /// Publishers purchase ISBNs from an affiliate of the International ISBN Agency. See https://en.wikipedia.org/wiki/International_Standard_Book_Number
+    /// </summary>
+    [Serializable]
+    public struct Isbn
+        : IEquatable<Isbn>
+        , IConvertible
+        , IXmlSerializable
+    {
+        private readonly string _isbn;
+
+        private readonly string _prefix;
+        private readonly string _identifierGroup;
+
+        private static readonly Regex _isbnExpression = new Regex(@"^(?<numbers>(\d-?)+)$");
+
+
+        /// <summary>
+        /// GS-1 prefix, only specified when this is a 13 character Isbn
+        /// </summary>
+        public string Prefix => _prefix;
+
+        /// <summary>
+        /// Registration Group Identifier
+        /// </summary>
+        public string IdentifierGroup => _identifierGroup;
+
+        /// <summary>
+        /// Create a new Isbn based on the given text.
+        /// </summary>
+        /// <param name="isbn">the text that represents the Isbn</param>
+        public Isbn(string isbn)
+        {
+            if (string.IsNullOrEmpty(isbn))
+            {
+                _isbn = isbn;
+                _prefix = null;
+                _identifierGroup = null;
+            }
+            else
+            {
+                var match = _isbnExpression.Match(isbn);
+                if (match.Success)
+                {
+                    var numbers = match.Groups["numbers"].Value?.Replace("-", "");
+                    if (numbers.Length == 10 || numbers.Length == 13)
+                    {
+                        _isbn = isbn;
+                        _prefix = numbers.Length == 10 ? null : numbers.Substring(0, 3);
+                        _identifierGroup = ExtractIdentifierGroup(numbers.Substring(numbers.Length - 10));
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(isbn));
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(isbn));
+                }
+            }
+        }
+
+        private static string ExtractIdentifierGroup(string isbnWithoutPrefix)
+        {
+            if (isbnWithoutPrefix == null) return null;
+
+            var c1 = isbnWithoutPrefix.Substring(0, 1);
+            var c2 = isbnWithoutPrefix.Substring(0, 2);
+            var c3 = isbnWithoutPrefix.Substring(0, 3);
+            var c4 = isbnWithoutPrefix.Substring(0, 4);
+
+            var i1 = int.Parse(c1, CultureInfo.InvariantCulture);
+            var i2 = int.Parse(c2, CultureInfo.InvariantCulture);
+            var i3 = int.Parse(c3, CultureInfo.InvariantCulture);
+            var i4 = int.Parse(c4, CultureInfo.InvariantCulture);
+
+            if (i4 >= 9990) return isbnWithoutPrefix.Substring(0, 5);
+            else if (i3 >= 990) return c4;
+            else if (i2 >= 95) return c3;
+            else if (i2 >= 80) return c2;
+            else if (i1 == 6) return c3;
+            else return c1;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => Equals((Isbn)obj);
+
+        /// <inheritdoc/>
+        public bool Equals(Isbn other) => string.Equals(_isbn, other._isbn, StringComparison.InvariantCulture);
+
+        /// <inheritdoc/>
+        public override int GetHashCode() => _isbn?.GetHashCode() ?? 0;
+
+        /// <inheritdoc/>
+        public override string ToString() => _isbn;
+
+        /// <inheritdoc/>
+        public static bool operator ==(Isbn lhs, Isbn rhs) => lhs.Equals(rhs);
+        /// <inheritdoc/>
+        public static bool operator !=(Isbn lhs, Isbn rhs) => !lhs.Equals(rhs);
+
+        /// <summary>
+        /// Cast the Isbn to a string
+        /// </summary>
+        public static explicit operator string(Isbn isbn) => isbn.ToString();
+
+        /// <inheritdoc />
+        XmlSchema IXmlSerializable.GetSchema() => null;
+
+        /// <inheritdoc />
+        void IXmlSerializable.ReadXml(XmlReader reader)
+        {
+            if (reader == null) throw new ArgumentNullException(nameof(reader));
+
+            var value = reader.MoveToAttribute("value") ? reader.Value : null;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                System.Runtime.CompilerServices.Unsafe.AsRef(this) = default;
+            }
+            else
+            {
+                System.Runtime.CompilerServices.Unsafe.AsRef(this) = new Isbn(value);
+            }
+
+            reader.Skip();
+        }
+
+        /// <inheritdoc />
+        void IXmlSerializable.WriteXml(XmlWriter writer)
+        {
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
+
+            if (!string.IsNullOrEmpty(_isbn)) writer.WriteAttributeString("value", _isbn);
+        }
+
+        /// <summary>
+        /// Try to create a new Isbn from the text. Returns true when succeeds.
+        /// </summary>
+        /// <param name="text">The text to be parsed</param>
+        /// <param name="result">the resulting Isbn</param>
+        public static bool TryParse(string text, out Isbn result)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                result = new Isbn(text);
+                return true;
+            }
+            var sanitized = text.Replace("-", "");
+            if (sanitized.Length == 10 || sanitized.Length == 13)
+            {
+                if (_isbnExpression.IsMatch(text))
+                {
+                    result = new Isbn(text);
+                    return true;
+                }
+            }
+            result = new Isbn();
+            return false;
+        }
+
+        TypeCode IConvertible.GetTypeCode() => TypeCode.Object;
+        bool IConvertible.ToBoolean(IFormatProvider provider) => Convert.ToBoolean(_isbn, provider);
+        byte IConvertible.ToByte(IFormatProvider provider) => Convert.ToByte(_isbn, provider);
+        char IConvertible.ToChar(IFormatProvider provider) => Convert.ToChar(_isbn, provider);
+        DateTime IConvertible.ToDateTime(IFormatProvider provider) => Convert.ToDateTime(_isbn, provider);
+        decimal IConvertible.ToDecimal(IFormatProvider provider) => Convert.ToDecimal(_isbn, provider);
+        double IConvertible.ToDouble(IFormatProvider provider) => Convert.ToDouble(_isbn, provider);
+        short IConvertible.ToInt16(IFormatProvider provider) => Convert.ToInt16(_isbn, provider);
+        int IConvertible.ToInt32(IFormatProvider provider) => Convert.ToInt32(_isbn, provider);
+        long IConvertible.ToInt64(IFormatProvider provider) => Convert.ToInt64(_isbn, provider);
+        sbyte IConvertible.ToSByte(IFormatProvider provider) => Convert.ToSByte(_isbn, provider);
+        float IConvertible.ToSingle(IFormatProvider provider) => Convert.ToSingle(_isbn, provider);
+        string IConvertible.ToString(IFormatProvider provider) => Convert.ToString(_isbn, provider);
+        object IConvertible.ToType(Type conversionType, IFormatProvider provider) => Convert.ChangeType(_isbn, conversionType, provider);
+        ushort IConvertible.ToUInt16(IFormatProvider provider) => Convert.ToUInt16(_isbn, provider);
+        uint IConvertible.ToUInt32(IFormatProvider provider) => Convert.ToUInt32(_isbn, provider);
+        ulong IConvertible.ToUInt64(IFormatProvider provider) => Convert.ToUInt64(_isbn, provider);
+
+    }
+}
