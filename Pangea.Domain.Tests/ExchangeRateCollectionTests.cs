75e83abfac5cc398718be971a30fb11c39306111
Ruben Rorije <rubenrorije@hotmail.com>
2019-09-13 10:03:22 +0200

    Add exchange rate collection (halfway)

---
+++ Pangea.Domain.Tests/ExchangeRateCollectionTests.cs
@@ -0,0 +1,97 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pangea.Domain.DefaultCurrencies;
+namespace Pangea.Domain.Tests
+{
+    [TestClass]
+    public class ExchangeRateCollectionTests
+    {
+        [TestMethod]
+        public void No_Conversion_Specified_Assumes_Same_Rate()
+        {
+            var sut = new ExchangeRateCollection();
+            sut.ConversionType.Should().Be(ExchangeRateConversionType.SameRateBothWays);
+        }
+
+        [TestMethod]
+        public void The_ConversionType_Specified_Must_Be_Stored()
+        {
+            var sut = new ExchangeRateCollection(ExchangeRateConversionType.InverseRateIsDifferent);
+            sut.ConversionType.Should().Be(ExchangeRateConversionType.InverseRateIsDifferent);
+        }
+
+        [TestMethod]
+        public void Convenient_Construction_With_Initializer()
+        {
+            var sut = new ExchangeRateCollection
+            {
+                new ExchangeRate(EUR, USD, 5m),
+                new ExchangeRate(EUR,AUD,10m)
+            };
+
+            sut.Count.Should().Be(2);
+        }
+        [TestMethod]
+        public void Cannot_Add_The_Same_Rate_Twice()
+        {
+            Action action = () => new ExchangeRateCollection
+            {
+                new ExchangeRate(EUR, USD, 5m),
+                new ExchangeRate(EUR, USD, 5m)
+            };
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void Contains_From_Currency_Not_Null()
+        {
+            Action action = () => new ExchangeRateCollection().Contains(null, EUR);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Contains_To_Currency_Not_Null()
+        {
+            Action action = () => new ExchangeRateCollection().Contains(EUR, null);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Contains_For_Currencies_When_Using_Same_Rates_Both_Ways()
+        {
+            var sut = new ExchangeRateCollection(ExchangeRateConversionType.SameRateBothWays)
+            {
+                new ExchangeRate(EUR, USD, 5m),
+            };
+            sut.Contains(EUR, USD).Should().BeTrue();
+            sut.Contains(EUR, AUD).Should().BeFalse();
+            sut.Contains(USD, EUR).Should().BeTrue();
+        }
+        [TestMethod]
+        public void Contains_For_Currencies_When_Using_Different_Rates_Both_Ways()
+        {
+            var sut = new ExchangeRateCollection(ExchangeRateConversionType.InverseRateIsDifferent)
+            {
+                new ExchangeRate(EUR, USD, 5m),
+            };
+            sut.Contains(EUR, USD).Should().BeTrue();
+            sut.Contains(USD, EUR).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Cannot_Add_The_Inverted_Rate_When_Using_The_Same_Rate_Both_Ways()
+        {
+            Action action = () => new ExchangeRateCollection(ExchangeRateConversionType.SameRateBothWays)
+            {
+                new ExchangeRate(EUR, USD, 5m),
+                new ExchangeRate(USD, EUR, 5m)
+            };
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ Pangea.Domain/ExchangeRateCollection.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pangea.Domain
+{
+    /// <summary>
+    /// Holds a number of exchange rates to do easy conversions between currencies
+    /// </summary>
+    public class ExchangeRateCollection : ICollection<ExchangeRate>
+    {
+        private readonly List<ExchangeRate> _exchangeRates;
+
+        /// <summary>
+        /// The type of conversion that is used in this instance
+        /// </summary>
+        public ExchangeRateConversionType ConversionType { get; }
+
+        /// <summary>
+        /// The number of elements in this collection
+        /// </summary>
+        public int Count => _exchangeRates.Count;
+
+        /// <summary>
+        /// Whether the collection is readonly (false)
+        /// </summary>
+        public bool IsReadOnly => false;
+
+
+        /// <summary>
+        /// Create an Exchange rate collection and uses the same rate both ways
+        /// </summary>
+        public ExchangeRateCollection() : this(ExchangeRateConversionType.SameRateBothWays)
+        {
+        }
+
+        /// <summary>
+        /// Create an Exchange rate collection
+        /// </summary>
+        /// <param name="conversionType">Specifies the type of conversion that is used</param>
+        public ExchangeRateCollection(ExchangeRateConversionType conversionType)
+        {
+            ConversionType = conversionType;
+            _exchangeRates = new List<ExchangeRate>();
+        }
+        /// <summary>
+        /// Adds an exchange rate to the end of this collection. 
+        /// 
+        /// </summary>
+        /// <param name="rate">the rate to add</param>
+        /// <exception cref="ArgumentNullException">When the <paramref name="rate"/> is null</exception>
+        /// <exception cref="ArgumentException">When a <paramref name="rate"/> for the same currencies is already added</exception>
+        public void Add(ExchangeRate rate)
+        {
+            if (rate == null) throw new ArgumentNullException(nameof(rate));
+            if (Contains(rate))throw new ArgumentException(Resources ,nameof(rate))
+            _exchangeRates.Add(rate);
+        }
+
+        /// <summary>
+        /// Returns the exchange rate for the given currencies.
+        /// </summary>
+        /// <param name="from">The currency to convert from</param>
+        /// <param name="to">The currency to convert to</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">When either currency is null</exception>
+        public bool Contains(Currency from, Currency to)
+        {
+            if (from == null) throw new ArgumentNullException(nameof(from));
+            if (to == null) throw new ArgumentNullException(nameof(to));
+
+            if (_exchangeRates.Any(rate => rate.From == from && rate.To == to))
+            {
+                return true;
+            }
+            else if (ConversionType == ExchangeRateConversionType.SameRateBothWays)
+            {
+                return _exchangeRates.Any(rate => rate.To == from && rate.From == to);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the exchange rates
+        /// </summary>
+        public IEnumerator<ExchangeRate> GetEnumerator() => _exchangeRates.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        /// <inheritdoc/>
+        public void Clear() => _exchangeRates.Clear();
+
+        /// <inheritdoc/>
+        public bool Contains(ExchangeRate item) => _exchangeRates.Contains(item);
+
+        /// <inheritdoc/>
+        public void CopyTo(ExchangeRate[] array, int arrayIndex) => _exchangeRates.CopyTo(array, arrayIndex);
+
+        /// <inheritdoc/>
+        public bool Remove(ExchangeRate item) => _exchangeRates.Remove(item);
+    }
+}
