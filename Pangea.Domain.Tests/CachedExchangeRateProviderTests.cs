ed3bb2b2ad8e33dfffee740876bec91244feaa5c
Ruben Rorije <rubenrorije@hotmail.com>
2019-10-21 11:05:23 +0200

    Add cached exchange rates

---
+++ Pangea.Domain.Tests/CachedExchangeRateProviderTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pangea.Domain.DefaultCurrencies;
+
+namespace Pangea.Domain.Tests
+{
+    [TestClass]
+    public class CachedExchangeRateProviderTests
+    {
+        [TestMethod]
+        public void Inner_Provider_Cannot_Be_Null()
+        {
+            Action action = () => new CachedExchangeRateProvider(null);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Getting_ExchangeRate_Twice_Only_Queries_The_Internal_Provider_Once()
+        {
+            var inner = new DummyProvider();
+            var cached = new CachedExchangeRateProvider(inner);
+
+            inner.Counter.Should().Be(0);
+
+            cached.TryGet(EUR, USD);
+            inner.Counter.Should().Be(1);
+            cached.TryGet(EUR, USD);
+            inner.Counter.Should().Be(1);
+            cached.TryGet(EUR, CAD);
+            inner.Counter.Should().Be(2);
+        }
+
+
+        private class DummyProvider : IExchangeRateProvider
+        {
+            public int Counter { get; private set; }
+
+            public ExchangeRate TryGet(Currency source, Currency target)
+            {
+                Counter++;
+                return null;
+            }
+        }
+    }
+
+
+}
---
+++ Pangea.Domain/CachedExchangeRateProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pangea.Domain
+{
+    /// <summary>
+    /// Add a wrapper around an <see cref="IExchangeRateProvider"/> to cache the results
+    /// </summary>
+    public class CachedExchangeRateProvider : IExchangeRateProvider
+    {
+        private readonly IExchangeRateProvider _provider;
+        private readonly ConcurrentDictionary<Tuple<Currency, Currency>, ExchangeRate> _rates;
+
+        /// <summary>
+        /// Create a cached version of the given <paramref name="provider"/>
+        /// </summary>
+        /// <param name="provider">The inner provider</param>
+        public CachedExchangeRateProvider(IExchangeRateProvider provider)
+        {
+            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+            _rates = new ConcurrentDictionary<Tuple<Currency, Currency>, ExchangeRate>();
+        }
+
+        /// <inheritdoc/>
+        public ExchangeRate TryGet(Currency source, Currency target)
+        {
+            var key = Tuple.Create(source, target);
+            return _rates.GetOrAdd(key, key => _provider.TryGet(key.Item1, key.Item2));
+        }
+    }
+}
