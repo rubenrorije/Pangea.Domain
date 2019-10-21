0c82b6fdd022b70897db4dce81c700345083b663
Ruben Rorije <rubenrorije@hotmail.com>
2019-10-21 10:46:02 +0200

    Added the IExchangeRateProvider back in

---
+++ Pangea.Domain/IExchangeRateProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Pangea.Domain
+{
+    /// <summary>
+    /// Most basic interface to create a way to get exchange rates from a source
+    /// </summary>
+    public interface IExchangeRateProvider
+    {
+        /// <summary>
+        /// Try to find an exchange rate to convert <paramref name="source"/> to <paramref name="target"/>
+        /// </summary>
+        /// <param name="source">The currency to convert from</param>
+        /// <param name="target">The currency to convert to</param>
+        /// <returns>The exchange rate when found, <c>null</c> otherwise</returns>
+        ExchangeRate TryGet(Currency source, Currency target);
+    }
+}
