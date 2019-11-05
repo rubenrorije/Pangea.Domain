04bc241dd4ec78a58c80aab99621e6dd04ec20b2
Ruben Rorije <rubenrorije@hotmail.com>
2019-11-05 11:22:20 +0100

    Angle class added

---
+++ Pangea.Domain.Tests/AngleTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangea.Domain.Tests
+{
+    [TestClass]
+    public class AngleTests
+    {
+        [TestMethod]
+        public void Empty_Angle_Is_Zero()
+        {
+            var sut = new Angle();
+
+            sut.Degrees.Should().Be(0);
+        }
+
+    }
+}
---
+++ Pangea.Domain/Angle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pangea.Domain
+{
+    /// <summary>
+    /// Represents an angle in degrees
+    /// </summary>
+    public struct Angle
+        : IEquatable<Angle>
+    {
+        /// <summary>
+        /// The degrees of the Angle
+        /// </summary>
+        public int Degrees { get; }
+
+        /// <summary>
+        /// Create a new Angle based on the number of degrees (0-360)
+        /// </summary>
+        /// <param name="degrees">The degrees (0-360)</param>
+        public Angle(int degrees)
+        {
+            Degrees = degrees;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return Equals((Angle)obj);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return Degrees.GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(Angle other)
+        {
+            return Degrees == other.Degrees;
+        }
+
+        /// <inheritdoc/>
+        public static bool operator ==(Angle left, Angle right) => left.Equals(right);
+
+        /// <inheritdoc/>
+        public static bool operator !=(Angle left, Angle right)=> !(left == right);
+    }
+}
