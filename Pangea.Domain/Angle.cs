d5e812411f52d1cebe86afb64b7d8a3692fa75c2
Ruben Rorije <rubenrorije@hotmail.com>
2019-11-05 11:34:29 +0100

    Add interface implementations to implement crosscutting tests

--- Pangea.Domain/Angle.cs
+++ Pangea.Domain/Angle.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace Pangea.Domain
 {
     /// <summary>
     /// Represents an angle in degrees
     /// </summary>
+    [Serializable]
     public struct Angle
         : IEquatable<Angle>
+        , IFormattable
+        , IXmlSerializable
+        , IConvertible
     {
         /// <summary>
         /// The degrees of the Angle
@@ -43,9 +51,89 @@ namespace Pangea.Domain
         }
 
         /// <inheritdoc/>
+        public override string ToString()
+        {
+            return ToString(null, null);
+        }
+
+        /// <inheritdoc/>
+        public string ToString(string format)
+        {
+            return ToString(format, null);
+        }
+
+        /// <inheritdoc />
+        XmlSchema IXmlSerializable.GetSchema() => null;
+
+        /// <inheritdoc />
+        void IXmlSerializable.ReadXml(XmlReader reader)
+        {
+            if (reader == null) throw new ArgumentNullException(nameof(reader));
+
+            var value = reader.MoveToAttribute("value") ? reader.Value : null;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                System.Runtime.CompilerServices.Unsafe.AsRef(this) = default;
+            }
+            else
+            {
+                System.Runtime.CompilerServices.Unsafe.AsRef(this) = new Angle(int.Parse(value, CultureInfo.InvariantCulture));
+            }
+
+            reader.Skip();
+        }
+
+        /// <inheritdoc />
+        void IXmlSerializable.WriteXml(XmlWriter writer)
+        {
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
+
+            writer.WriteAttributeString("value", Degrees.ToString(null, CultureInfo.InvariantCulture));
+        }
+
+        /// <inheritdoc/>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return Degrees.ToString(format, formatProvider);
+        }
+
+
+        /// <inheritdoc/>
         public static bool operator ==(Angle left, Angle right) => left.Equals(right);
 
         /// <inheritdoc/>
-        public static bool operator !=(Angle left, Angle right)=> !(left == right);
+        public static bool operator !=(Angle left, Angle right) => !(left == right);
+
+        /// <summary>
+        /// Extract the raw degrees value from the angle
+        /// </summary>
+        /// <param name="angle">The angle</param>
+        public static explicit operator int(Angle angle) => angle.ToInt32();
+
+
+        /// <summary>
+        /// Extract the raw degrees value from the angle
+        /// </summary>
+        public int ToInt32() => Degrees;
+
+        TypeCode IConvertible.GetTypeCode() => TypeCode.Object;
+        bool IConvertible.ToBoolean(IFormatProvider provider) => Convert.ToBoolean(Degrees, provider);
+        byte IConvertible.ToByte(IFormatProvider provider) => Convert.ToByte(Degrees, provider);
+        char IConvertible.ToChar(IFormatProvider provider) => Convert.ToChar(Degrees, provider);
+        DateTime IConvertible.ToDateTime(IFormatProvider provider) => Convert.ToDateTime(Degrees, provider);
+        decimal IConvertible.ToDecimal(IFormatProvider provider) => Convert.ToDecimal(Degrees, provider);
+        double IConvertible.ToDouble(IFormatProvider provider) => Convert.ToDouble(Degrees, provider);
+        short IConvertible.ToInt16(IFormatProvider provider) => Convert.ToInt16(Degrees, provider);
+        int IConvertible.ToInt32(IFormatProvider provider) => Convert.ToInt32(Degrees, provider);
+        long IConvertible.ToInt64(IFormatProvider provider) => Convert.ToInt64(Degrees, provider);
+        sbyte IConvertible.ToSByte(IFormatProvider provider) => Convert.ToSByte(Degrees, provider);
+        float IConvertible.ToSingle(IFormatProvider provider) => Convert.ToSingle(Degrees, provider);
+        string IConvertible.ToString(IFormatProvider provider) => Convert.ToString(Degrees, provider);
+        object IConvertible.ToType(Type conversionType, IFormatProvider provider) => Convert.ChangeType(Degrees, conversionType, provider);
+        ushort IConvertible.ToUInt16(IFormatProvider provider) => Convert.ToUInt16(Degrees, provider);
+        uint IConvertible.ToUInt32(IFormatProvider provider) => Convert.ToUInt32(Degrees, provider);
+        ulong IConvertible.ToUInt64(IFormatProvider provider) => Convert.ToUInt64(Degrees, provider);
+
     }
 }
